74caaff2fe9586d89d680dd1a2965c95061637c5
SamyCode <sambea2103@gmail.com>
2023-05-12 11:39:30 -0400

    All Pages Created

---
+++ src/template/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    const view = `
+    <div class="Haeder-main">
+        <div class="Header-logo">
+            <h1><a href="/">Rick & Morty</a></h1>
+        </div>
+        <div class="Header-nav">
+            <a href="#/about/">About</a>
+        </div>
+    </div>
+    `
+    return view
+}
+
+export default Header
